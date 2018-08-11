14c1d99c09cff34bd0bfc49e0ebeab5a95e5c577
Jake <35958607+OneTrueLegend@users.noreply.github.com>
2018-08-11 15:29:13 +1200

    Update python_controller.py
    Added gui and cmd based input file selector

--- python_controller.py
+++ python_controller.py
@@ -19,7 +19,25 @@ class Controller(Cmd):
     def do_output_to_png(self, args):
         """Convert dot file into PNG"""
         call(['dot', '-Tpng', 'tmp/class.dot', '-o', 'tmp/class.png'])
+    
+    #Created by Jake Reddock
+    def do_set_input_file(self, args):
+        """
+        Sets the input file that will be converted into a UML diagram.
 
+        set_input_file [file_name]
+
+        """
+        if len(args) == 0:
+            root = Tk()
+            root.filename = filedialog.askopenfilename(initialdir="C:/", title="Select Input File",
+                                                       filetypes=(("Python Files", "*.py"), ("all files", "*.*")))
+            self.args = root.filename
+            root.withdraw()
+        else:
+            self.args = args
+        print("Input file selected \"" + self.args + "\"")
+    
     @staticmethod
     def run_parser(file_names):
         # Initiate processor
