1fccfa8c1cd6c881459f5f4bf90a19651bb1eb47
Jake <35958607+OneTrueLegend@users.noreply.github.com>
2018-08-15 12:42:20 +1200

    Create statistics_handler.py

---
+++ statistics_handler.py
@@ -0,0 +1,37 @@
+import sql
+
+
+class ClassData:
+    def __init__(self, class_name, attribute_count, method_count):
+        self.class_name = class_name
+        self.attribute_count = attribute_count
+        self.method_count = method_count
+
+
+class StatisticsHandler:
+    def __init__(self):
+        self.db = sql.database("example")
+
+    def create_tables(self):
+        self.db.query("CREATE TABLE IF NOT EXISTS ClassData ("
+                      "classID INTEGER NOT NULL AUTO_INCREMENT "
+                      "className VARCHAR(255)"
+                      "attributeCount INTEGER "
+                      "methodCount INTEGER"
+                      ")")
+
+    def insert_class(self, class_name, attribute_count, method_count):
+        self.db.query("INSERT INTO ClassData VALUES('" +
+                      class_name + "','" +
+                      attribute_count + "','" +
+                      method_count + "');")
+
+    def get_class_data(self):
+        class_data_list = []
+        result = self.db.query("SELECT className,attributeCount,methodCount").fetch()
+        for row in result:
+            class_name = row['className']
+            attribute_count = row['attributeCount']
+            method_count = row['methodCount']
+            class_data_list.append(ClassData(class_name, attribute_count, method_count))
+        return class_data_list
