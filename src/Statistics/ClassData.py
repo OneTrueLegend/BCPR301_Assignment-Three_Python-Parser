4c8e2ed1819c7fcdd7add2e7d8556420b903109b
Jake <reddock.jake@gmail.com>
2018-10-30 22:16:29 +1300

    Factory Method: StatisticsCreator converted into a factory which creates a graph product which is then used to create a graph

---
+++ src/Statistics/ClassData.py
@@ -0,0 +1,18 @@
+# By Jake Reddock
+class ClassData:
+    """
+    Class Data object containing attributes
+    Author: Jake
+
+    >>> ClassData("TestClass",1,1).class_name
+    'TestClass'
+    >>> ClassData("TestClass",1,1).attribute_count
+    1
+    >>> ClassData("TestClass",1,1).method_count
+    1
+    """
+
+    def __init__(self, class_name, attribute_count, method_count):
+        self.class_name = class_name
+        self.attribute_count = attribute_count
+        self.method_count = method_count
